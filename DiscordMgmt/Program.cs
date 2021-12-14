a59f70e3a02ac9a3586527dc97d50d1782e4a70d
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-12-14 12:40:02 +1100

    Initial commit

---
+++ DiscordMgmt/Program.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Rest;
+
+const string ApiKeyName = "DISCORD_APIKEY";
+
+string? apiKey = Environment.GetEnvironmentVariable(ApiKeyName);
+if(apiKey == null)
+{
+    throw new InvalidOperationException($"{ ApiKeyName } environment variable missing or empty");
+}
+
+using DiscordRestClient client = new DiscordRestClient();
+await client.LoginAsync(Discord.TokenType.Bot, apiKey, true);
+IReadOnlyCollection<RestGuild> guilds = await client.GetGuildsAsync();
+
