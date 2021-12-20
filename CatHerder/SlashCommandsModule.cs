03a9603542fa8bc962922a7a9543b8c15cdf7036
Anthony Langsworth <anthony.langsworth@gmail.com>
2021-12-20 18:27:16 +1100

    First working version of the slash command

--- CatHerder/SlashCommandsModule.cs
+++ CatHerder/SlashCommandsModule.cs
@@ -1,7 +1,5 @@
 ﻿using Discord;
 using Discord.Interactions;
-using Discord.Rest;
-using Discord.Webhook;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
@@ -13,7 +11,7 @@ namespace CatHerder
 {
     public class SlashCommandsModule: InteractionModuleBase<InteractionContext>
     {
-        [SlashCommand("permissions-report", "Construct a report listing server users and their role memberships")]
+        [SlashCommand("permissions-report", "Send a report listing server users and their role memberships to the caller via direct message")]
         //[RequireUserPermission(GuildPermission.ManageRoles | GuildPermission.ManageChannels,
         //    ErrorMessage = "You lack required permissions to request this report.")]
         public async Task PermissionsReport()
@@ -22,16 +20,19 @@ namespace CatHerder
 
             try
             {
+                // Context.Guild is null for some reason
+                SocketGuild guild = ((SocketGuildUser) Context.User).Guild;
+
                 // Requires SERVER MEMBERS INTENT to be enabled for the bot. Otherwise,
                 // this line will hang. 
-                await Context.Guild.DownloadUsersAsync();
-                    
-                IEnumerable<IRole> roles = Context.Guild.Roles.OrderBy(role => role.Position);
+                await guild.DownloadUsersAsync();
+
+                IEnumerable<IRole> roles = guild.Roles.OrderBy(role => role.Position);
 
                 using MemoryStream memoryStream = new MemoryStream();
                 using StreamWriter streamWriter = new StreamWriter(memoryStream);
                 streamWriter.WriteLine("User," + string.Join(",", roles.Select(role => EscapeForCSV(role.Name))));
-                foreach (IGuildUser user in (await Context.Guild.GetUsersAsync()).OrderBy(user => user.Nickname ?? user.Username))
+                foreach (IGuildUser user in guild.Users.OrderBy(user => user.Nickname ?? user.Username))
                 {
                     streamWriter.WriteLine(EscapeForCSV(user.Nickname ?? user.Username) + ","
                         + string.Join(",", roles.Select(role => user.RoleIds.Any(roleId => roleId == role.Id) ? "Y" : "N")));
@@ -45,14 +46,14 @@ namespace CatHerder
                     await dmChannel.SendFileAsync(
                         stream: memoryStream,
                         filename: "permissions.csv",
-                        text: "I have attached a report listing the server users and their role memberships.");
+                        text: "I have attached a report listing the server's users and their role memberships.");
                 }
                 finally
                 {
                     await dmChannel.CloseAsync();
                 }
 
-                await Context.Interaction.ModifyOriginalResponseAsync(messageProperties => messageProperties.Content = "I have responded via direct message.");
+                await Context.Interaction.ModifyOriginalResponseAsync(messageProperties => messageProperties.Content = "I attached the report to a direct message.");
             }
             catch
             {
